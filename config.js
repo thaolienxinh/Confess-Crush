96b388326f004b6016681d3ac490d1cf3df81f1d
thaolienxinh <118123817+thaolienxinh@users.noreply.github.com>
2022-11-14 10:00:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi một chút tình iu",
+    introTitle: 'Chồng iu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
